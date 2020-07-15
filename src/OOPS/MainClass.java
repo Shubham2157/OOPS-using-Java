98f10da1f458d5af74d398965f44201ac9aabd4c
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-07-15 17:00:02 +0530

    Initial commit

---
+++ src/OOPS/MainClass.java
@@ -0,0 +1,67 @@
+package OOPS;
+
+import javafx.css.Size;
+
+class Cat{
+    boolean isfur;
+    String color, breeds;
+    int lags, eyes;
+
+    public void walk()
+    {
+        System.out.println("Cat can walk");
+    }
+    public void eat(){
+        System.out.println("cat can eat");
+    }
+    public void description()
+    {
+        System.out.println("Cat Has "+ lags + " legs and " + eyes +" eyes");
+    }
+}
+
+class Dog
+{
+    String name, bread;
+
+    public void jump()
+    {
+        System.out.println("My Dog " + name + " jumped");
+    }
+    public void Description()
+    {
+        System.out.println("My Dog name is " + name + " and its breed is " + bread);
+    }
+}
+
+public class MainClass {
+    public static void main(String[] args) {
+//        Cat cat1 = new Cat();
+//        Cat cat2 = new Cat();
+//
+//        cat1.walk();
+//        cat2.eat();
+//
+//        cat1.lags = 4;
+//        cat1.eyes = 2;
+//
+//        cat1.description();
+//        cat2.description();
+        Dog husky = new Dog();
+        Dog poodle = new Dog();
+
+        husky.bread = "Husky";
+        husky.name = "Browny";
+
+        poodle.bread = "Poodle";
+        poodle.name = "Mr. Moolchand Ji";
+
+        husky.jump();
+        husky.Description();
+
+        poodle.jump();
+        poodle.Description();
+
+       // System.out.println();
+    }
+}
