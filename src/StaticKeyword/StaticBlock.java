434a1ca46a421c9b92c7a4d50feeb10c7748c675
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-07-18 11:42:20 +0530

    Static Block added

--- src/StaticKeyword/StaticBlock.java
+++ src/StaticKeyword/StaticBlock.java
@@ -1,4 +1,16 @@
 package StaticKeyword;
 
 public class StaticBlock {
+    static {
+        System.out.println("Block 1 executed"); // static block executed first then main fun is executed
+    }
+    static {
+        System.out.println("Block 2 executed");
+    }
+
+    public static void main(String[] args) {
+
+            System.out.println("Main fun executed executed");
+
+    }
 }
